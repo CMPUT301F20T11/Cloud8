df2f6c7861eb99147d05aadb96991fbc70775748
Edlee Ducay <55123805+EdleeDucay@users.noreply.github.com>
2020-10-26 14:05:44 -0600

    Book & BookAdapter entities

--- app/src/main/java/com/example/booktracker/boundary/BookAdapter.java
+++ app/src/main/java/com/example/booktracker/boundary/BookAdapter.java
@@ -1,4 +1,70 @@
 package com.example.booktracker.boundary;
 
-public class BookAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.booktracker.R;
+import com.example.booktracker.entities.Book;
+
+import java.util.ArrayList;
+
+public class BookAdapter extends ArrayAdapter<Book> {
+    private ArrayList<Book> bookList = new ArrayList<>();
+    private Context context;
+
+    /**
+     * Constructor for BookAdapter class
+     * @param argContext
+     * @param argBookList
+     */
+    public BookAdapter(Context argContext, ArrayList<Book> argBookList) {
+        super(argContext, 0, argBookList);
+        this.bookList = argBookList;
+        this.context = argContext;
+    }
+
+    /**
+     * Binds layout inflater to a list view
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return the view for corresponding params
+     */
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.book_adapter_content, parent, false);
+        }
+
+        Book book = bookList.get(position);
+
+        TextView bookTitle = view.findViewById(R.id.title_text);
+        TextView bookAuthor = view.findViewById(R.id.author_text);
+        TextView bookOwner = view.findViewById(R.id.owner_text);
+        TextView bookStatus = view.findViewById(R.id.status_text);
+
+        StringBuilder authors = new StringBuilder();
+        for (String s: book.getAuthor()) {
+            authors.append(s);
+            authors.append(", ");
+        }
+
+        bookTitle.setText(book.getTitle());
+        bookAuthor.setText(authors.toString());
+        bookOwner.setText(book.getOwner());
+        bookStatus.setText(book.getStatus());
+
+
+        return view;
+    }
+
 }
--- app/src/main/java/com/example/booktracker/entities/Book.java
+++ app/src/main/java/com/example/booktracker/entities/Book.java
@@ -1,4 +1,88 @@
 package com.example.booktracker.entities;
 
-public class Book {
+import java.io.Serializable;
+import java.util.List;
+
+public class Book implements Serializable {
+    private List<String> author;
+    private String title;
+    private int isbn;
+    private String description;
+    private String owner;
+    private String status;
+
+    /**
+     * Constructor for books with a description
+     * @param argAuthor
+     * @param argTitle
+     * @param argIsbn
+     * @param argDesc
+     */
+    public Book(List<String>argAuthor, String argTitle, int argIsbn, String argDesc) {
+        this.author = argAuthor;
+        this.title = argTitle;
+        this.isbn = argIsbn;
+        this.description = argDesc;
+    }
+
+    /**
+     * Constructor for books without a description
+     * @param argAuthor
+     * @param argTitle
+     * @param argIsbn
+     */
+    public Book(List<String>argAuthor, String argTitle, int argIsbn) {
+        this.author = argAuthor;
+        this.title = argTitle;
+        this.isbn = argIsbn;
+    }
+
+    public List<String> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(List<String> author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
 }
