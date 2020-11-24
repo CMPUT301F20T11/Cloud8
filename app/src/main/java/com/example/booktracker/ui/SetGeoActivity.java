e9603fd97c4478e70b5fc2df64269161d4f65533
Ryan <moro@ualberta.ca>
2020-11-24 03:28:54 -0700

    Implemented gps permission cases

--- app/src/main/java/com/example/booktracker/ui/SetGeoActivity.java
+++ app/src/main/java/com/example/booktracker/ui/SetGeoActivity.java
@@ -8,45 +8,49 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.booktracker.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.GeoPoint;
 
 import static android.content.ContentValues.TAG;
 
 public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private boolean mLocationPermissionGranted = false;
+    private boolean userGPS = false;
     public static final int ERROR_DIALOG_REQUEST = 9001;
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9003;
     private static final float DEFAULT_ZOOM = 10;
     private FusedLocationProviderClient mFusedLocationClient;
+    final Marker[] marker = {null};
+    Marker pickupMarker = marker[0];
+    Double pickupLat = null;
+    Double pickupLng = null;
+    private GoogleMap map;
 
     //edmonton
     LatLng defaultLocation = new LatLng(53.5461, -113.4938);
@@ -63,44 +67,73 @@ public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallb
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        Button cancelBtn = findViewById(R.id.geo_cancel_button);
+        cancelBtn.setOnClickListener(view -> {
+            Intent returnIntent = new Intent();
+            setResult(RESULT_CANCELED, returnIntent);
+            finish();
+        });
+
+        Button confirmBtn = findViewById(R.id.geo_confirm_button);
+        confirmBtn.setOnClickListener(view -> {
+            if (pickupMarker != null) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("pickupLat", pickupLat);
+                returnIntent.putExtra("pickupLng", pickupLng);
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            } else {
+                Toast.makeText(this, "No location selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkGPS();
+
     }
 
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    public void onMapReady(GoogleMap retMap) {
+        map = retMap;
+//        checkGPS();
+        if (!mLocationPermissionGranted) {
             Log.d(TAG, "Current location is unavailable. Using defaults.");
             map.moveCamera(CameraUpdateFactory
                     .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
             map.getUiSettings().setMyLocationButtonEnabled(false);
-        }
-        else{
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
             map.setMyLocationEnabled(true);
-            getCurrentLocation(map);
+            getCurrentLocation();
         }
+        Toast.makeText(this, "Hold location to place pickup spot", Toast.LENGTH_SHORT).show();
 
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                map.addMarker(new MarkerOptions()
-                        .position(latLng)
-                        .title("Pickup location"));
-                //confirm/edit location with user
-                //attach location LATLONG to accepted request
-                //return to incoming requests
+                pickupLat = latLng.latitude;
+                pickupLng = latLng.longitude;
+                if (pickupMarker != null) {
+                    pickupMarker.setPosition(latLng);
+                } else {
+                    pickupMarker = map.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Pickup location"));
+                }
             }
         });
-
     }
 
-    private void getCurrentLocation(GoogleMap map) {
+    private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Location location = task.getResult();
                     Double lat = location.getLatitude();
                     Double lon = location.getLongitude();
@@ -114,78 +147,57 @@ public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallb
     }
 
 
-    /**
-     * gps permission checks taken from
-     * https://github.com/mitchtabian/Google-Maps-2018/blob/google-services-gps-and-location-permission-end/app/src/main/java/com/codingwithmitch/googlemaps2018/ui/MainActivity.java
-     * checks location permissions
-     * added in calls for setGeoActivity functionality
-     * TODO: allow for non gps enabled devices to display map of edmonton without current location pin
-     */
+    public void checkGPS() {
+        if (isServicesOK()) {
+            if (isGPSEnabled()) {
+                if (mLocationPermissionGranted) {
+                    userGPS = true;
+                } else {
+                    getLocationPermission();
+                }
+            }
+        }
+    }
 
-    public boolean isServicesOK(){
+
+    public boolean isServicesOK() {
         Log.d(TAG, "isServicesOK: checking google services version");
 
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(SetGeoActivity.this);
 
-        if(available == ConnectionResult.SUCCESS){
+        if (available == ConnectionResult.SUCCESS) {
             //everything is fine and the user can make map requests
             Log.d(TAG, "isServicesOK: Google Play Services is working");
             return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             //an error occured but we can resolve it
             Log.d(TAG, "isServicesOK: an error occured but we can fix it");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(SetGeoActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }else{
+        } else {
             Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(checkMapServices()){
-            if(mLocationPermissionGranted){
-                //
-            }
-            else{
-                getLocationPermission();
-            }
+    public boolean isGPSEnabled() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+            return false;
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d(TAG, "onActivityResult: called.");
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ENABLE_GPS: {
-                if(mLocationPermissionGranted){
-                    //
-                }
-                else{
-                    getLocationPermission();
-                }
-            }
-        }
-
-    }
-
-    private boolean checkMapServices(){
-        if(isServicesOK()){
-            if(isMapsEnabled()){
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 
     private void buildAlertMessageNoGps() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
-                .setCancelable(false)
+        builder.setMessage("Enable GPS?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        userGPS = false;
+                        // start activity without gps functionality
+                    }
+                })
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
                         Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
@@ -197,13 +209,18 @@ public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallb
         alert.show();
     }
 
-    public boolean isMapsEnabled(){
-        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-            buildAlertMessageNoGps();
-            return false;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if (!mLocationPermissionGranted) {
+                    getLocationPermission();
+                }
+            }
         }
-        return true;
+
     }
 
     /**
@@ -220,6 +237,8 @@ public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallb
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
+            //start activity with gps functionality
+
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
@@ -242,9 +261,22 @@ public class SetGeoActivity extends AppCompatActivity implements OnMapReadyCallb
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mLocationPermissionGranted = true;
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    map.setMyLocationEnabled(true);
+                    getCurrentLocation();
                 }
             }
         }
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (!mLocationPermissionGranted) {
+//            getLocationPermission();
+//        }
+//    }
 
 }
